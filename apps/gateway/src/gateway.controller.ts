746968f8c6f4ea33472baf26968e7e8f842cc440
Deepu <itsdeeputiwari@gmail.com>
2026-01-16 22:47:57 +0530

    make some changes

--- apps/gateway/src/gateway.controller.ts
+++ apps/gateway/src/gateway.controller.ts
@@ -1,16 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 import { GatewayService } from './gateway.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class GatewayController {
-  constructor(private readonly gatewayService: GatewayService) {}
+  constructor() {}
 
   @Get('health')
-  getHealth() {
-    return {
-      ok:true,
-      service:'Gateway',
-      now: new Date().toLocaleDateString()
-    };
+  async health(){
+    const ping=async(serviceName:string, client:ClientProxy)=>{
+      try {
+        const result=await firstValueFrom(
+          client.send('service.ping',{from:'gateway'})
+        )
+        return {
+          ok:true,
+          service:serviceName,
+          result
+        }
+      } catch (error) {
+        return {
+          ok:false,
+          service:serviceName,
+          error:error?.message??"Unknown error",
+        }
+      }
+    }
+
   }
 }
